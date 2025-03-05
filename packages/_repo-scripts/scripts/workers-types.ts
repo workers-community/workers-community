d8845198e099220a21ec5c577a51109d864cece0
Dániel Földi <dani@stefan.hu>
2025-03-05 22:16:54 +0000

    :hammer: Some fixes

--- packages/_repo-scripts/scripts/workers-types.ts
+++ packages/_repo-scripts/scripts/workers-types.ts
@@ -1,20 +1,19 @@
 import assert from 'node:assert'
-import { exists } from 'node:fs'
 import { copyFile, mkdir, readdir, readFile, rm, writeFile } from 'node:fs/promises'
 import { resolve } from 'node:path'
-import { promisify } from 'node:util'
 import { x } from 'tar'
 import { temporaryFile } from 'tempy'
 
-
-const existsPromise = promisify(exists)
 
 // find the latest version of @cloudflare/workers-types from npm
 const versionResponse = await fetch('https://registry.npmjs.org/@cloudflare/workers-types')
 const version = await (versionResponse.json() as Promise<{'dist-tags': {latest: string}}>).then(data => data['dist-tags'].latest)
 console.log(version)
 
-const packageFolder = resolve(import.meta.dirname,'..', '..', 'workers-types')
+// fail for other major version so we can manually investigate
+assert.strictEqual(version.split('.')[0], '4', 'version should be 4.x.x')
+
+const packageFolder = resolve(import.meta.dirname, '..', '..', 'workers-types')
 
 // download decompress into packages/workers-types
 const download = await fetch(`https://registry.npmjs.org/@cloudflare/workers-types/-/workers-types-${version}.tgz`)
@@ -22,7 +21,7 @@ const tarballBuffer = await download.arrayBuffer()
 const tarball = temporaryFile({ extension: 'tar.gz' })
 await writeFile(tarball, new Uint8Array(tarballBuffer))
 await mkdir(packageFolder, { recursive: true })
-await x({ file: tarball, cwd: packageFolder, strip: 1, "keep-existing": true })
+await x({ file: tarball, cwd: packageFolder, strip: 1, 'keep-existing': true })
 console.log(packageFolder)
 
 // replace package.json name with "@workers-community/workers-types"
@@ -35,10 +34,24 @@ packageJson.repository = {
 }
 packageJson.author = 'Workers Community'
 
+/* FOR FIXES
+// bump patch version if already published
+const communityPackage = await fetch(`https://registry.npmjs.org/${packageJson.name}`)
+if (communityPackage.ok) {
+  const { versions } = await communityPackage.json() as { versions: Record<string, unknown | undefined> }
+  if (version in versions) {
+    const [ major, minor, patch ] = version.split('.').map(Number)
+    assert.ok(typeof patch === 'number', 'Invalid package version')
+    packageJson.version = `${major}.${minor}.${patch + 1}`
+  }
+}
+*/
+
 // find folder with newest date name
 const newestEntrypoint = (await readdir(packageFolder)).filter(file => /^\d{4}-\d{2}-\d{2}/.test(file)).sort((a, b) => {
   const aDate = new Date(a)
   const bDate = new Date(b)
+
   return bDate.getTime() - aDate.getTime()
 })[0]
 
@@ -61,13 +74,14 @@ for await (const entrypoint of await readdir(packageFolder, { withFileTypes: tru
   if (!entrypoint.isDirectory()) {
     continue
   }
+
   await rm(resolve(packageFolder, entrypoint.name), { recursive: true })
 
   console.log(entrypoint.name)
 
   packageJson.exports[`./${entrypoint.name}`] = {
-    types: `./${entrypoint.name}/index.d.ts`,
-    import: `./${entrypoint.name}/index.ts`
+    types: './index.d.ts',
+    import: './index.ts'
   }
 }
 
