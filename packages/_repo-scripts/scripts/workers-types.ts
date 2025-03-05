a3b6848ef66a7a116adba2066a31378c17e5458c
Dániel Földi <dani@stefan.hu>
2025-03-05 21:49:38 +0000

    :zap: Add workers-types alternative

---
+++ packages/_repo-scripts/scripts/workers-types.ts
@@ -0,0 +1,75 @@
+import assert from 'node:assert'
+import { exists } from 'node:fs'
+import { copyFile, mkdir, readdir, readFile, rm, writeFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { promisify } from 'node:util'
+import { x } from 'tar'
+import { temporaryFile } from 'tempy'
+
+
+const existsPromise = promisify(exists)
+
+// find the latest version of @cloudflare/workers-types from npm
+const versionResponse = await fetch('https://registry.npmjs.org/@cloudflare/workers-types')
+const version = await (versionResponse.json() as Promise<{'dist-tags': {latest: string}}>).then(data => data['dist-tags'].latest)
+console.log(version)
+
+const packageFolder = resolve(import.meta.dirname,'..', '..', 'workers-types')
+
+// download decompress into packages/workers-types
+const download = await fetch(`https://registry.npmjs.org/@cloudflare/workers-types/-/workers-types-${version}.tgz`)
+const tarballBuffer = await download.arrayBuffer()
+const tarball = temporaryFile({ extension: 'tar.gz' })
+await writeFile(tarball, new Uint8Array(tarballBuffer))
+await mkdir(packageFolder, { recursive: true })
+await x({ file: tarball, cwd: packageFolder, strip: 1, "keep-existing": true })
+console.log(packageFolder)
+
+// replace package.json name with "@workers-community/workers-types"
+const packageJsonFile = await readFile(resolve(packageFolder, 'package.json'), 'utf8')
+const packageJson = JSON.parse(packageJsonFile)
+packageJson.name = '@workers-community/workers-types'
+packageJson.repository = {
+  type: 'git',
+  url: 'https://github.com/workers-community/workers-types'
+}
+packageJson.author = 'Workers Community'
+
+// find folder with newest date name
+const newestEntrypoint = (await readdir(packageFolder)).filter(file => /^\d{4}-\d{2}-\d{2}/.test(file)).sort((a, b) => {
+  const aDate = new Date(a)
+  const bDate = new Date(b)
+  return bDate.getTime() - aDate.getTime()
+})[0]
+
+assert.ok(newestEntrypoint, 'No entrypoint candidates found')
+console.log(newestEntrypoint)
+
+packageJson.exports = {
+  '.': {
+    types: './index.d.ts',
+    import: './index.ts'
+  }
+}
+
+// copy newest entrypoint to root
+await copyFile(resolve(packageFolder, newestEntrypoint, 'index.d.ts'), resolve(packageFolder, 'index.d.ts'))
+await copyFile(resolve(packageFolder, newestEntrypoint, 'index.ts'), resolve(packageFolder, 'index.ts'))
+
+// add export overrides and remove all entrypoints in folders
+for await (const entrypoint of await readdir(packageFolder, { withFileTypes: true })) {
+  if (!entrypoint.isDirectory()) {
+    continue
+  }
+  await rm(resolve(packageFolder, entrypoint.name), { recursive: true })
+
+  console.log(entrypoint.name)
+
+  packageJson.exports[`./${entrypoint.name}`] = {
+    types: `./${entrypoint.name}/index.d.ts`,
+    import: `./${entrypoint.name}/index.ts`
+  }
+}
+
+await writeFile(resolve(packageFolder, 'package.json'), JSON.stringify(packageJson, null, 2))
+await rm(resolve(packageFolder, 'entrypoints.svg'))
---
+++ packages/workers-types/README.md
@@ -0,0 +1,9 @@
+# @workers-community/workers-types
+
+## Install
+
+```bash
+npm install -D @workers-community/workers-types
+-- Or
+yarn add -D @workers-community/workers-types
+```
